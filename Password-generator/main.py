aab392ea233265d62bd6f20fda70e46ecc878553
Sajeesh-mohanan <127338485+Sajeesh-mohanan@users.noreply.github.com>
2023-07-20 21:26:47 +0530

    Add files via upload

---
+++ Password-generator/main.py
@@ -0,0 +1,102 @@
+from tkinter import *
+from tkinter import messagebox
+import random
+import pyperclip
+
+LETTERS = 'a A b B c C d D e E f F g G h H i I j J k K l M n N o O p Q r R s S t T u U v V w W x X y Y z Z'.split()
+
+NUMBERS = '1 2 3 4 5 6 7 8 9 0'.split()
+
+SYMBOLS = '! @ # $ % ^ & * ( ) _ +'.split()
+
+
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+def generate_password():
+    global LETTERS
+    global NUMBERS
+    global SYMBOLS
+    flag = 0
+    while flag != 1:
+        password = ''
+        password_list = [random.choice(LETTERS) for i in range(random.randint(5, 9))]
+
+        password_list += [random.choice(NUMBERS) for i in range(random.randint(2, 4))]
+
+        password_list += [random.choice(SYMBOLS) for i in range(random.randint(1, 3))]
+
+        random.shuffle(password_list)
+
+        for i in password_list:
+            password += i
+            if i.isupper():
+                flag = 1
+
+    psw_entry.delete(0, END)
+    psw_entry.insert(0, password)
+    pyperclip.copy(password)
+
+
+# ---------------------------- SAVE PASSWORD ------------------------------- #
+def save_password():
+    website = website_entry.get()
+    email = email_entry.get()
+    password = psw_entry.get()
+
+    if website == "" or password == "":
+        messagebox.showerror(title="Error", message="Please don't leave empty fields")
+
+    else:
+        is_ok = messagebox.askokcancel(title=f"{website}", message=f"Email: {email} \nPassword: {password} \n Do you want to save?")
+
+        if is_ok:
+            with open("data.txt", mode="a") as file:
+                file.write(f"{website} | {email} | {password} \n")
+
+            website_entry.delete(0, END)
+            psw_entry.delete(0, END)
+
+
+# ---------------------------- UI SETUP ------------------------------- #
+
+# Window setup
+window = Tk()
+window.config(pady=50, padx=50)
+window.title("MyPassword Manager")
+
+img = PhotoImage(file="logo.png")   #-----image file------#
+
+# Canvas
+canvas = Canvas(height=200, width=200)
+canvas.create_image(110, 100, image=img) #----Image Canvas-----#
+canvas.grid(row=0, column=1)
+
+# Website Entry
+website_label = Label(text="Website: ")
+website_label.grid(row=1, column=0)
+website_entry = Entry(width=52)
+website_entry.focus()
+website_entry.grid(row=1, column=1, columnspan=2)
+
+# Email label
+email_label = Label(text="Email/Username: ")
+email_label.grid(row=2, column=0)
+email_entry = Entry(width=52)
+email_entry.insert(0, "sajeesh.mohan29@gmail.com")
+email_entry.grid(row=2, column=1, columnspan=2)
+
+# Password Label
+psw_label = Label(text="Password: ")
+psw_label.grid(row=3, column=0)
+psw_entry = Entry(width=33)
+psw_entry.grid(row=3, column=1)
+
+
+# Generate password button
+gen_pass_btn = Button(text="Generate Password", width=14, command=generate_password)
+gen_pass_btn.grid(row=3, column=2)
+
+# Add Button
+add_btn = Button(text="Add", width=44, command=save_password)
+add_btn.grid(row=4, column=1, columnspan=2)
+
+Tk.mainloop(window)
